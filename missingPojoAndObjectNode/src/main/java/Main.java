aaaf1c5d94d348c607efee7df28bdc2b7d9a9b09
Alex Plate <AlexPl292@gmail.com>
2018-09-21 14:57:44 +0300

    MissingNode for jackson answer
    - https://stackoverflow.com/questions/52436391/passing-missingnode-to-objectmapper-to-convert-to-pojo

---
+++ missingPojoAndObjectNode/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * Created by Alex Plate on 21.09.2018.
+ * https://stackoverflow.com/questions/52436391/passing-missingnode-to-objectmapper-to-convert-to-pojo
+ */
+public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Pojo[] arr = new Pojo[2];
+        arr[0] = new Pojo(1);
+        arr[1] = new Pojo(1);
+        ObjectNode root = mapper.createObjectNode();
+        JsonNode pojoJson = root.withArray("Test");
+        mapper.treeToValue(pojoJson, Pojo[].class);
+    }
+}
---
+++ missingPojoAndObjectNode/src/main/java/Pojo.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+/**
+ * Created by Alex Plate on 21.09.2018.
+ */
+public class Pojo {
+    int number;
+
+    public Pojo(int x) {
+        this.number = new Random().nextInt();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
